111912dcae77ab7aa9b1474703ef01fb57842468
Wren Steitle <lauren.steitle@gmail.com>
2019-10-02 23:00:23 -0600

    Commit pirate class and tests

--- mythical-creatures/lib/pirate.rb
+++ mythical-creatures/lib/pirate.rb
@@ -1,8 +1,30 @@
 class Pirate
-attr_reader :name
+attr_reader :name, :job
+attr_accessor :cursed, :heinous_acts, :booty
 
-  def initialize(name)
+  def initialize(name, job = "Scallywag")
     @name = name
+    @job = job
+    @cursed = false
+    @heinous_acts = 0
+    @booty = 0
+  end
+
+  def cursed?
+    @cursed
+  end
+
+  def commit_heinous_act
+    @heinous_acts = @heinous_acts += 1
+    if @heinous_acts >= 3
+      @cursed = true
+    end
+    @heinous_acts
+  end
+
+  def rob_ship
+    @booty += 100
   end 
+
 
 end
--- mythical-creatures/test/pirate_test.rb
+++ mythical-creatures/test/pirate_test.rb
@@ -15,25 +15,21 @@ class PirateTest < Minitest::Test
   end
 
   def test_is_a_scallywag_by_default
-    skip
     pirate = Pirate.new("Jack")
     assert_equal 'Scallywag', pirate.job
   end
 
   def test_in_not_always_a_scallywag
-    skip
     pirate = Pirate.new("Jack", "Cook")
     assert_equal "Cook", pirate.job
   end
 
   def test_isnt_cursed_by_default
-    skip
     pirate = Pirate.new("Jack")
     refute pirate.cursed?
   end
 
   def test_becomes_cursed_after_enough_heinous_acts
-    skip
     pirate = Pirate.new("Jack")
     refute pirate.cursed?
     pirate.commit_heinous_act
@@ -45,16 +41,23 @@ class PirateTest < Minitest::Test
   end
 
   def test_a_pirate_has_booty
-    skip
     # create a pirate
     # check that the pirate starts with 0 booty
+    pirate = Pirate.new("Red Beard")
+    assert_equal 0, pirate.booty
   end
 
   def test_a_pirate_gets_100_booty_for_robbing_ships
-    skip
     # create a pirate
     # rob some ships
     # check that the pirate got 100 booty for each ship it robbed
+    pirate = Pirate.new("Davy Jones")
+    pirate.rob_ship
+    assert_equal 100, pirate.booty
+    pirate.rob_ship
+    assert_equal 200, pirate.booty
+    pirate.rob_ship
+    assert_equal 300, pirate.booty
   end
 
 end
